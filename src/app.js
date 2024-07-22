ca5a1a8c502ee1076d1bdb10ec5345479c0411cd
luiscastillaPro <159273408+luiscastillaPro@users.noreply.github.com>
2024-07-22 11:00:53 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,35 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+  const suits = ["Corazones", "Picas", "Tréboles", "Diamantes"];
+  const values = [
+    "As",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jota",
+    "Reina",
+    "Rey"
+  ];
+
+  function getRandomElement(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  function generateRandomCard() {
+    const suit = getRandomElement(suits);
+    const value = getRandomElement(values);
+    return `${value} de ${suit}`;
+  }
+
+  const cardElement = document.getElementById("card");
+  cardElement.textContent = generateRandomCard();
+});
